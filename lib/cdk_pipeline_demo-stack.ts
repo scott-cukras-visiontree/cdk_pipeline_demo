3a4362371770eb9a6df5e63bc11a90e48fc91977
scott cukras <scukras@visiontree.com>
2023-11-13 12:45:28 -0800

    update

--- lib/cdk_pipeline_demo-stack.ts
+++ lib/cdk_pipeline_demo-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,16 @@ export class CdkPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPipelineDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    /*const pipeline = */ new CodePipeline(this, 'DemoPipeline', {
+      pipelineName: 'DemoPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('scott-cukras-visiontree/cdk_pipeline_demo', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      })
+    })
   }
 }
