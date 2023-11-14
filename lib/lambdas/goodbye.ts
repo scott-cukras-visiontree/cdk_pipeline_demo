28db3bafc6c00c8ed47391e57b558078a63287e0
scott cukras <scukras@visiontree.com>
2023-11-14 10:27:05 -0800

    add console logs to lambdas

--- lib/lambdas/goodbye.ts
+++ lib/lambdas/goodbye.ts
@@ -2,5 +2,6 @@ import { Message } from "./message.types"
 
 export const goodbye = (): Message => {
   const message = `Goodbye from ${process.env.DEPLOYMENT_STAGE}`
+  console.log('MESSAGE: ', message)
   return { message }
 }
--- lib/lambdas/hello.ts
+++ lib/lambdas/hello.ts
@@ -2,5 +2,6 @@ import { Message } from "./message.types"
 
 export const hello = (): Message => {
   const message = `Hello from ${process.env.DEPLOYMENT_STAGE}`
+  console.log('MESSAGE: ', message)
   return { message }
 }
